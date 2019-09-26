12596b534147eabe0d98ff9a1d0a0710a99f4111
Junaid Muringattu <junuaidm@gmail.com>
2019-09-26 10:29:28 +0530

    Project Setup

---
+++ Exotic.BLL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Exotic.BLL
+{
+    public class Class1
+    {
+    }
+}
---
+++ Exotic.DAL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Exotic.DAL
+{
+    public class Class1
+    {
+    }
+}
